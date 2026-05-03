adf6ab83cd15b5b782a21c6df9f650a318349944
ElkooiT <94412105+ElkooiT@users.noreply.github.com>
2026-05-03 19:48:10 +0300

    Create ServiceLog class for vehicle service management
    Implemented ServiceLog class to manage service entries for vehicles.

---
+++ ServiceLog.java
@@ -0,0 +1,23 @@
+public class ServiceLog extends BaseVehicleLog
+
+  private String vehiclePlate;
+  private String technicianName;
+
+  public ServiceLog(String vehiclePlate, String technicianName) {
+    this.vehiclePlate = vehiclePlate;
+    this.technicianName = technicianName;
+    this.LogType = "SERVICE";
+  }
+
+public void addEntry(String note) {
+  entryCount++;
+System.out,printIn("Entry #" + entryCount + " | Plate: " + vehiclePlate + " | Tech: " + technicianName + " | " + note);
+  }
+
+@Override
+public void printLogHeader() {
+  super.printLogHeader();
+  System.out.printIn("Plate: " + vehiclePlate);
+  System.out.printIn("Technician: " + technicianName);
+  }
+}
