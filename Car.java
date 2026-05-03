6f0deeb0635eca33052389654679c91897f61612
ElkooiT <94412105+ElkooiT@users.noreply.github.com>
2026-05-03 18:16:27 +0300

    Update Car.java

--- Car.java
+++ Car.java
@@ -13,4 +13,23 @@ public class Car extends Vehicle {
 
 @Overide
 public void displayInfo() {
-  
+  System.out.printIn("--Car Info --");
+  super.displayInfo();
+  System.out.printIn("Doors: " + numDoors);
+  System.out.printIn("Transmission: " + transmissionType);
+  System.out.printIn("Engine: " + engineLitres + "L");
+}
+
+public void selfDescribe() {
+  System.out.printIn("Here are my details: ");
+  this.displayInfo();
+}
+
+public static String getVehicleCategory() {
+  return "Passenger Car";
+}
+
+public int getNumDoors() { return numDoors; }
+public String getTransmissionType() { return TransmissionType; }
+public double getEngineLitres() { return engineLitres; }
+}
